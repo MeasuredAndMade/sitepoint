4cbf45a1f2d2f68319ba12a2c2b7c623ba22ec5e
Measured & Made <measuredandmade@outlook.com>
2026-02-10 18:39:13 -0500

    Created Update User Controller and added if a user is promoted or demoted and the corresponding activity with the creator database

---
+++ backend/src/controllers/updateUser.js
@@ -0,0 +1,73 @@
+import { supabase } from "../services/supabase";
+
+export const updateUser = async (req, res) => {
+    const userId = req.params.id;
+    const { name, username, role } = req.body;
+
+    // Fetch the existing user
+    const { data: existingUser, error: fetchError } = await supabase
+        .from('users')
+        .select('id, role_id')
+        .eq('id', userId)
+        .single();
+    
+    if (fetchError) {
+        console.log("FETCH USER ERROR:" , fetchError);
+        return res.status(500).json({ error: fetchError.message });
+    }
+
+    // Update User
+    const { data: updateUser, error: updateError } = await supabase
+        .from('users')
+        .update({
+            name,
+            username,
+            role_id: role
+        })
+        .eq('id', userId)
+        .select()
+        .single();
+
+    if (updateError) {
+        console.log('UPDATE USER ERROR:', updateError);
+        return res.status(500).json({ error: updateError.message });
+    }
+
+    // If role changed from user -> admin, create creator record
+    const becameAdmin = existingUser.role_id !== 1 && role === 1;
+
+    if (becameAdmin) {
+        const { error: creatorError } = await supabase
+            .from('creators')
+            .insert([
+                {
+                    user_id: userId,
+                    name: name
+                }
+            ]);
+        
+        if (creatorError) {
+            console.log("CREATOR INSERT ERROR: ", creatorError);
+            return res.status(500).json({ error: creatorError.message });
+        }
+    }
+
+    const demoted = existingUser.role_id === 1 && role !== 1;
+
+    if (demoted) {
+        const { error: deleteError } = await supabase
+            .from('creators')
+            .delete()
+            .eq('user_id', userId);
+
+        if (deleteError) {
+            console.log('CREATE DELETE ERROR: ', deleteError);
+            return res.status(500).json({ error: deleteError.message });
+        }
+    }
+
+    return res.status(200).json({
+        message: 'User updated succesfully',
+        user: updateUser
+    });
+};
