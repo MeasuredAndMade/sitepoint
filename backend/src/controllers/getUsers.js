dc4ac5072c0fc4a19b1b625b684a9395af201fa5
Measured & Made <measuredandmade@outlook.com>
2026-02-10 19:23:41 -0500

    getUsers.js created

---
+++ backend/src/controllers/getUsers.js
@@ -0,0 +1,15 @@
+import { supabase } from "../services/supabase"
+
+export const getUsers = async (req, res) => {
+    const { data, error } = await supabase
+        .from('users')
+        .select('id, name, username, role_id')
+        .order('id', { ascending: true });
+
+    if (error) {
+        console.log('GET USERS ERROR:', error);
+        return res.status(500).json({ error: error.message });
+    }
+
+    return res.satus(200).json(data);
+}
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { createUser } from '../controllers/userController.js';
+import { updateUser } from '../controllers/updateUser.js';
+import { getUsers } from '../controllers/getUsers.js';
 
 const router = express.Router();
 
 router.post('/create-user', createUser);
+router.put('/update-user', updateUser);
+router.get('/users', getUsers);
 
 export default router;
